4133746cf17aa6f333e4be84d6bd6ca38fbdaf63
Marshal Great <31458994+dev-great@users.noreply.github.com>
2021-08-09 16:13:01 +0100

    updat

--- Django_portfolio_app/settings.py
+++ Django_portfolio_app/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'account',
-    'contact',
     'portfolio',
 ]
 
--- Django_portfolio_app/urls.py
+++ Django_portfolio_app/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from contact import views as contact_views
 from django.conf.urls.static import static
 from django.conf import settings
 
