26ffd5ffb1c4c0741d0b1bc511db7e2660a1448c
Marshal Great <31458994+dev-great@users.noreply.github.com>
2021-08-09 16:05:46 +0100

    up

--- contact/models.py
+++ contact/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Contact(models.Model):
     email = models.EmailField()
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     message = models.TextField()
 
     def __str__(self):
