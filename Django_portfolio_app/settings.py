d883f34cdd5c2df3ccecbf484a377a7dde1c4987
Marshal Great <31458994+dev-great@users.noreply.github.com>
2021-08-09 16:16:52 +0100

    o

--- Django_portfolio_app/settings.py
+++ Django_portfolio_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 import dj_database_url
-from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,7 +81,7 @@ WSGI_APPLICATION = 'Django_portfolio_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
