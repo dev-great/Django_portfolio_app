bc398414879d1553d2bdc8eb3254eb476d19ca14
Marshal Great <31458994+dev-great@users.noreply.github.com>
2021-08-10 09:11:03 +0100

    up

--- Django_portfolio_app/settings.py
+++ Django_portfolio_app/settings.py
@@ -126,18 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[(os.path.join(BASE_DIR, 'static'))]
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
-
-
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-
-from django.contrib.messages import constants as messages
-MESSAGE_TAGS = {
-    messages.ERROR: 'danger',
-
-}
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
