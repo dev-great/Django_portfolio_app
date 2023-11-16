e3697fe4b67a30d6c7df83926ec8243f0ae6b864
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2023-11-16 23:35:50 +0100

    update

--- Django_portfolio_app/settings.py
+++ Django_portfolio_app/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '8*+jtux04i%ld!l+si&i#_xhs@10j9ewri+7j4ic3n3-zhswu@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['greamhouse.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['greatmarshal-1778d18eaeed.herokuapp.com',
+                 '127.0.0.1', 'greatdev.xyz']
 
 
 # Application definition
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'Django_portfolio_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR , 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[(os.path.join(BASE_DIR, 'static'))]
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
@@ -134,4 +135,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 if 'DATABASE_URL' in os.environ:
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
-
